36d57815f793550dce7841700bcbadd45fd41908
temoncher <temoncher@yandex.ru>
2020-08-21 22:07:41 +0300

    delete console.log

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { SingleStageTest } from './single-stage-test';
 
-const newTest = new SingleStageTest([1, 4, 0, 7, 2, 3, 5, 6]);
-
-const interpretations = newTest.interpret();
-
-console.log(interpretations);
-
 export { SingleStageTest };
 
 // TODO: write tests
