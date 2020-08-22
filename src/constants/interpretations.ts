e813d9c19fac2e50a14c4972d9e0127da5f82127
temoncher <temoncher@yandex.ru>
2020-08-22 21:06:58 +0300

    move full packs

--- src/constants/interpretations.ts
+++ src/constants/interpretations.ts
@@ -1,10 +1,10 @@
 import { Sign } from '@/constants/sign.enum';
 import { Interpretations } from '@/models/interpretations.type';
 
-import asteriskFunctionJson from '@/assets/i18n/asterisk-function.json';
-import equalFunctionJson from '@/assets/i18n/equal-function.json';
-import minusFunctionJson from '@/assets/i18n/minus-function.json';
-import plusFunctionJson from '@/assets/i18n/plus-function.json';
+import asteriskFunctionJson from '@/assets/i18n/multi/asterisk-function.json';
+import equalFunctionJson from '@/assets/i18n/multi/equal-function.json';
+import minusFunctionJson from '@/assets/i18n/multi/minus-function.json';
+import plusFunctionJson from '@/assets/i18n/multi/plus-function.json';
 
 export const interpretations: Interpretations = {
   [Sign.PLUS]: plusFunctionJson,
