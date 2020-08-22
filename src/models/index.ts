5f26f879862d9966554bae24685ee863ad4ef203
temoncher <temoncher@yandex.ru>
2020-08-22 16:05:45 +0300

    redo npmignore config

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+import { ColorGroups } from './color-groups.type';
+import { ColorResult } from './color-result.interface';
+import { InterpretationSection } from './interpretation-section.interface';
+import { MinusFunctionInterpretation } from '../interpretations/types/minus-function-interpretation.interface';
+
