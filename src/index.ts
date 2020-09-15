52816b9c480d793430a8b83a22d59d7aaad45aa3
temoncher <temoncher@yandex.ru>
2020-09-15 04:24:41 +0300

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ export { SingleStageTest } from './single-stage-test';
 export { TwoStageTest } from './two-stage-test';
 
 // Models
-export { SignMap as ColorGroups } from './types/sign-map.type';
+export { ColorMap } from './types/color-map.type';
 export { FunctionKeys } from './types/function-keys';
 export { InterpretationSection } from './types/interpretation-section.interface';
+export { Interpretation } from './types/interpretation.type';
 export { LuscherFunction } from './types/luscher-function.interface';
 export { MinusFunctionInterpretation } from './types/minus-function-interpretation.interface';
+export { SignMap } from './types/sign-map.type';
 export { Translations } from './types/translations.interface';
 
 // Enums
@@ -18,3 +20,5 @@ export { Sign } from './types/enums/sign.enum';
 
 // Constatns
 export { luscherColors, LuscherColor } from './constants/luscher-colors';
+export { badColors } from './constants/bad-colors';
+export { goodColors } from './constants/good-colors';
